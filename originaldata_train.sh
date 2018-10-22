656a5920a154f5b8a646b58e0b7f65fbf4283ea2
Manuel Mager <magerml@falke1.ims.uni-stuttgart.de>
2018-10-22 18:17:53 +0200

    UD

--- originaldata_train.sh
+++ originaldata_train.sh
@@ -2,4 +2,4 @@
 
 mkdir -p models
 
-python seg_rnn.py --train ../data/UD_French-ParTUT/fr_partut-ud-train.conllu --test ../data/UD_French-ParTUT/fr_partut-ud-test.conllu --modelPath models # --model seg_rnn_correct_0.pt
+python seg_rnn.py --train data/UD_French-ParTUT/fr_partut-ud-train.conllu --test data/UD_French-ParTUT/fr_partut-ud-test.conllu --modelPath models # --model seg_rnn_correct_0.pt
